771f39672002d0ddcd1dfece529f1735e87c1f1a
Fadwa Ali <75944731+Fadwahigga@users.noreply.github.com>
2024-06-10 00:28:13 +0300

    edit route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/student', [StudentController::class, 'getAllStudent']);
 Route::post('/student', [StudentController::class, 'addStudent']);
-Route::put('/student/edit/{id', [StudentController::class, 'editStudent']);
-Route::delete('/student/delete/{id', [StudentController::class, 'deleteStudent']);
+Route::put('/student/edit/{id}', [StudentController::class, 'editStudent']);
+Route::delete('/student/delete/{id}', [StudentController::class, 'deleteStudent']);
